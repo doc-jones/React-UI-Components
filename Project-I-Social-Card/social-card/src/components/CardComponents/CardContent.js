6feeeaf26fe0463994fe010d73ddc38b8d6d08db
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-04-22 16:52:05 -0400

    Add content to HeaderContent and styles to Card.css

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -4,6 +4,7 @@ import './Card.css';
 
 function CardContent() {
     <main>
+        <img src="../../public/img/reactdark.jpg"></img>
         <h3>Get Started with React</h3>
         <p>React makes it painless to created interactive UIs. Design simple views for each state in your application.</p>
         <a href="#">reactjs.org</a>
